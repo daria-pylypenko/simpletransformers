e6b236b7a55ba62bc55a24024762f7804cd926d6
Daria Pylypenko <pylypenko.daria@gmail.com>
2020-05-08 03:23:05 +0200

    Changed some variable names within the BertForMultiTaskSequenceClassification class

--- simpletransformers/custom_models/models.py
+++ simpletransformers/custom_models/models.py
@@ -79,14 +79,14 @@ class BertForMultiTaskSequenceClassification(BertPreTrainedModel):
     Currently 2 classification tasks with different numbers of classes
     """
     def __init__(self, config, weight=None):
-        super(BertForSequenceClassification, self).__init__(config)
-        self.num_labels_1 = config.num_labels
-        self.num_labels_2 = config.num_additional_labels
+        super(BertForMultiTaskSequenceClassification, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.num_additional_labels = config.num_additional_labels
 
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier_1 = nn.Linear(config.hidden_size, self.config.num_labels_1)
-        self.classifier_2 = nn.Linear(config.hidden_size, self.config.num_labels_2)
+        self.classifier_1 = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.classifier_2 = nn.Linear(config.hidden_size, self.config.num_additional_labels)
         self.weight = weight
 
         self.init_weights()
@@ -99,8 +99,8 @@ class BertForMultiTaskSequenceClassification(BertPreTrainedModel):
         token_type_ids=None,
         position_ids=None,
         head_mask=None,
-        labels_1=None,
-        labels_2=None
+        labels=None,
+        additional_labels=None
     ):
 
         outputs = self.bert(
@@ -126,8 +126,8 @@ class BertForMultiTaskSequenceClassification(BertPreTrainedModel):
             loss_fct_1 = CrossEntropyLoss(weight=self.weight)
             loss_fct_2 = CrossEntropyLoss() # for now will ignore the weights,
                                             # but in theory could be implemented
-            loss_1 = loss_fct_1(logits_1.view(-1, self.num_labels_1), labels_1.view(-1))
-            loss_2 = loss_fct_2(logits_2.view(-1, self.num_labels_2), labels_2.view(-1))
+            loss_1 = loss_fct_1(logits_1.view(-1, self.num_labels), labels.view(-1))
+            loss_2 = loss_fct_2(logits_2.view(-1, self.num_additional_labels), additional_labels.view(-1))
             loss = loss_1 + loss_2
             outputs = (loss,) + outputs
 
